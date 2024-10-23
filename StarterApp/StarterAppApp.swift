f14692b8d353f7ae3bb189565c1c6aa0878bed85
Dylan Neal <dyl.neal@gmail.com>
2024-10-23 14:40:24 -0500

    Initial Commit

---
+++ StarterApp/StarterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarterAppApp.swift
+//  StarterApp
+//
+//  Created by Dylan Neal on 10/23/24.
+//
+
+import SwiftUI
+
+@main
+struct StarterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StarterAppTests/StarterAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  StarterAppTests.swift
+//  StarterAppTests
+//
+//  Created by Dylan Neal on 10/23/24.
+//
+
+import Testing
+@testable import StarterApp
+
+struct StarterAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
